74ab2dd25c0a4eae6fdfa81175990062c4e63ad8
anil843324 <anilkrsingh14200@gmail.com>
2022-07-21 13:25:47 +0530

    almost complete

--- ecomm-project/src/components/Header.jsx
+++ ecomm-project/src/components/Header.jsx
@@ -1,30 +1,58 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useSelector ,useDispatch} from "react-redux";
+import { Link } from "react-router-dom";
+import {productSearch} from "../redux/productAction"
+import "./Header.css";
 const Header = () => {
+  const result = useSelector((state) => state.cartData);
 
-  const result = useSelector((state) => state.cartData)
+  const dispatch=useDispatch()
+ 
+   const [inputData,setInutData]=useState("")
 
-  console.log("All data", result)
+  const queryUser =(e) => {
+
+     setInutData(e.target.value)
+    dispatch(productSearch(inputData))
+
+   
+  };
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      queryUser();
+
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, [inputData]);
 
 
   return (
-    <div className='header'>
-      <Link to="/">  <h2 className='logo'>E-Comm</h2> </Link>
+    <div className="header">
+      <Link to="/">
+        {" "}
+        <h2 className="logo">E-Comm</h2>{" "}
+      </Link>
+      <div className="searchBox">
+        <input
+          type="text"
+          placeholder="Search Products"
+          value={inputData}
+          onChange={(e) => {queryUser(e)} }
+        />
+      </div>
       <Link to="/cart">
-        <div className='cart-div'>
-
+        <div className="cart-div">
           <span>{result.length}</span>
-          <img src="https://www.freeiconspng.com/thumbs/cart-icon/cart-icon-14.png" alt="logo" />
-
+          <img
+            src="https://www.freeiconspng.com/thumbs/cart-icon/cart-icon-14.png"
+            alt="logo"
+          />
         </div>
       </Link>
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- ecomm-project/src/redux/productSaga.js
+++ ecomm-project/src/redux/productSaga.js
@@ -1,19 +1,30 @@
 import { takeEvery, put } from "redux-saga/effects";
-import { PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
+import { PRODUCT_LIST, PRODUCT_SEARCH, SET_PRODUCT_LIST } from "./constant";
 
 function* getProducts() {
   let data = yield fetch("http://localhost:3004/products ");
   data = yield data.json();
-  // console.log("action is called", data);
   //    now call to send the data to action
   yield put({
     type: SET_PRODUCT_LIST,
     data,
   });
 }
+function* searchProducts(data) {
+  let filterData = yield fetch(`http://localhost:3004/products?q=${data.query} `);
+  filterData = yield filterData.json();
+  //    now call to send the data to action
+    console.log('action is called ',filterData)
+  yield put({
+    type: SET_PRODUCT_LIST,
+     data:filterData,
+  });
+}
+
 
 function* productSaga() {
   yield takeEvery(PRODUCT_LIST, getProducts);
+  yield takeEvery(PRODUCT_SEARCH, searchProducts);
 }
 
 export default productSaga;
